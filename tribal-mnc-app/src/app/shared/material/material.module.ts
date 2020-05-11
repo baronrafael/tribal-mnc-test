774e3a82ebaa2725ff4097dd6d289e857e7fc811
Rafael Baron <baronrafael@hotmail.com>
2020-05-11 18:13:28 -0400

    app: added material and shared modules

---
+++ tribal-mnc-app/src/app/shared/material/material.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+const MATERIAL_MODULES = [
+  CommonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatListModule,
+  MatButtonModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatSnackBarModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ...MATERIAL_MODULES
+  ],
+  exports: [
+    ...MATERIAL_MODULES
+  ]
+})
+export class MaterialModule { }
