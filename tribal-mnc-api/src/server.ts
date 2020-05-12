a1ad8ab8968cf91474c79e2619ae8f9c17578b8f
Rafael Baron <baronrafael@hotmail.com>
2020-05-12 10:10:11 -0400

    api: removing the connection to the database

--- tribal-mnc-api/src/server.ts
+++ tribal-mnc-api/src/server.ts
@@ -1,38 +1,33 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import routes from "./Routes/";
 import cors = require("cors");
 import helmet = require("helmet");
 
-createConnection().then(async connection => {
+const app = express();
 
-     
-   const app = express();
-
-    var whitelist = ['http://localhost:4200/']
-    var corsOptions = {
-      origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-      }
+var whitelist = ['http://localhost:4200/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
+  }
+}
 
 
-   // Call midlewares
-   app.use(cors());
-   app.use(helmet());
-   app.use(bodyParser.json());
+// Call midlewares
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
   
-   //Set all routes from routes folder
-   app.use("/", routes);
+//Set all routes from routes folder
+app.use("/", routes);
 
-   app.listen(3000, () => {
-     console.log("Server started on port 3000!");
+app.listen(3000, () => {
+  console.log("Server started on port 3000!");
 });
 
-}).catch(error => console.log(error));
