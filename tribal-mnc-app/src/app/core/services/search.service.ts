620501802b44b47944d392ffcef67001889a83e2
Rafael Baron <baronrafael@hotmail.com>
2020-05-11 20:12:39 -0400

    app: api and search services

--- tribal-mnc-app/src/app/core/services/search.service.ts
+++ tribal-mnc-app/src/app/core/services/search.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(private http: ApiService) { }
+
+  search(type: string, text: string){
+    return this.http.get(type + '/' + text)
+  }
+
 }
